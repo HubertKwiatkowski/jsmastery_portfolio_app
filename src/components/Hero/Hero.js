c22b51721ad944bfef4040d18c94342a542f10ae
Hubert <h.kwiatkowski@hotmail.com>
2023-01-20 18:06:12 +0100

    project section done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,19 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, maxime excepturi voluptatem nostrum necessitatibus non numquam mollitia fuga quae eius.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Learn More</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
